ff382b7b5253d7db316c8afc99aa3fddf7ebf732
Fernando Candiani <candianirfernando@gmail.com>
2022-12-13 11:39:53 -0300

    Component Message Bubble

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -6,14 +6,14 @@ import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
-  static final _defaultUser = ChatUser(
-    id: '1',
-    name: 'Teste',
-    email: 'teste@teste.com',
+  static const _defaultUser = ChatUser(
+    id: '456',
+    name: 'Ana',
+    email: 'ana@teste.com',
     imageURL: 'assets/images/avatar.png',
   );
 
-  static Map<String, ChatUser> _users = {
+  static final Map<String, ChatUser> _users = {
     _defaultUser.email: _defaultUser,
   };
   static ChatUser? _currentUser;
--- lib/core/services/chat/chat_service.dart
+++ lib/core/services/chat/chat_service.dart
@@ -5,7 +5,7 @@ import 'package:chat/core/services/chat/chat_mock_service.dart';
 abstract class ChatService {
   Stream<List<ChatMessage>> messagesStream();
   Future<ChatMessage> save(String text, ChatUser user);
-  
+
   factory ChatService() {
     return ChatMockService();
   }
